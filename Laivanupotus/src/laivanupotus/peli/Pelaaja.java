bc505e463f721e8c1397a556d3138fc0d836e645
unknown <maija.ohman@helsinki.fi>
2013-03-26 22:51:37 +0200

    taas neljas muokkaus

---
+++ Laivanupotus/src/laivanupotus/peli/Pelaaja.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laivanupotus.peli;
+
+/**
+ *
+ * @author mohman
+ */
+public class Pelaaja {
+    private String nimi;
+    
+    public Pelaaja(String nimi) {
+        this.nimi = nimi;
+    }
+    
+    public String getNimi() {
+        return this.nimi;
+    }
+    
+}
